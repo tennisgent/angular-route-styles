d05618fabdd5fa2b05158d2e877e18a21c1da9b8
Zack Boman <tennisgent@gmail.com>
2015-08-16 20:24:59 -0600

    Supports custom interpolation symbols
    Fixes issue #18 for ui-router

--- ui-route-styles.js
+++ ui-route-styles.js
@@ -5,66 +5,70 @@
  */
 
 (function(){
+	'use strict';
 
-    var mod = angular.module('routeStyles', ['ui.router']);
+    angular.module('routeStyles', ['ui.router']);
 
-    mod.directive('head', ['$rootScope','$compile',
-		function($rootScope, $compile){
-			return {
-				restrict: 'E',
-				link: function(scope, elem){
-					var html = '<link rel="stylesheet" ng-repeat="(routeCtrl, cssUrl) in routeStyles" ng-href="{{cssUrl}}" >';
-					elem.append($compile(html)(scope));
-					scope.routeStyles = {};
-					$rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
-						// Remove old styles
-						if (fromState && fromState.css) {
-							if(!angular.isArray(fromState.css)){
-								fromState.css = [fromState.css];
-							}
-							angular.forEach(fromState.css, function(sheet){
-								delete scope.routeStyles[sheet];
-							});
-						}
-						if(fromState && fromState.views){
-							angular.forEach(fromState.views, function(view){
-								if(view.css){
-									if(!angular.isArray(view.css)){
-										view.css = [view.css];
-									}
-									angular.forEach(view.css, function(sheet){
-										scope.routeStyles[sheet] = undefined;
-									});
+	    .directive('head', ['$rootScope','$compile','$interpolate',
+			function($rootScope, $compile, $interpolate){
+				// this allows for support of custom interpolation symbols
+                var startSym = $interpolate.startSymbol(),
+                    endSym = $interpolate.endSymbol(),
+                    html = ['<link rel="stylesheet" ng-repeat="(routeCtrl, cssUrl) in routeStyles" ng-href="',startSym,'cssUrl',endSym,'">'].join('');
+				return {
+					restrict: 'E',
+					link: function(scope, elem){
+						elem.append($compile(html)(scope));
+						scope.routeStyles = {};
+						$rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+							// Remove old styles
+							if (fromState && fromState.css) {
+								if(!angular.isArray(fromState.css)){
+									fromState.css = [fromState.css];
 								}
-							});
-						}
-						
-						// Add new styles
-						if(toState && toState.views){
-							angular.forEach(toState.views, function(view){
-								if(view.css){
-									if(!angular.isArray(view.css)){
-										view.css = [view.css];
-									}
-									angular.forEach(view.css, function(sheet){
-										scope.routeStyles[sheet] = sheet;
-									});
-									
-								}
-							});
-						}
-						if (toState && toState.css) {
-							if(!angular.isArray(toState.css)){
-								toState.css = [toState.css];
+								angular.forEach(fromState.css, function(sheet){
+									delete scope.routeStyles[sheet];
+								});
 							}
-							angular.forEach(toState.css, function(sheet){
-								scope.routeStyles[sheet] = sheet;
-							});
-						}
-					});
-				}
-			};
-		}
-	]);
+							if(fromState && fromState.views){
+								angular.forEach(fromState.views, function(view){
+									if(view.css){
+										if(!angular.isArray(view.css)){
+											view.css = [view.css];
+										}
+										angular.forEach(view.css, function(sheet){
+											scope.routeStyles[sheet] = undefined;
+										});
+									}
+								});
+							}
+							
+							// Add new styles
+							if(toState && toState.views){
+								angular.forEach(toState.views, function(view){
+									if(view.css){
+										if(!angular.isArray(view.css)){
+											view.css = [view.css];
+										}
+										angular.forEach(view.css, function(sheet){
+											scope.routeStyles[sheet] = sheet;
+										});
+										
+									}
+								});
+							}
+							if (toState && toState.css) {
+								if(!angular.isArray(toState.css)){
+									toState.css = [toState.css];
+								}
+								angular.forEach(toState.css, function(sheet){
+									scope.routeStyles[sheet] = sheet;
+								});
+							}
+						});
+					}
+				};
+			}
+		]);
 
 })();
